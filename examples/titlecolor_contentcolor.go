df87d73f19d9a8f28221731f4f70de79cfc2eb13
Delta456 <swstkbaranwal@gmail.com>
2022-02-06 17:05:31 +0530

    add more exaples

---
+++ examples/titlecolor_contentcolor.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Delta456/box-cli-maker/v2"
+)
+
+func main() {
+	StyleCases := []string{"Single", "Double", "Single Double", "Double Single", "Bold", "Round", "Hidden", "Classic"}
+	ColorTypes := []string{"Black", "Blue", "Red", "Green", "Yellow", "Cyan", "Magenta", "White", "HiBlack", "HiBlue", "HiRed", "HiGreen", "HiYellow", "HiCyan", "HiMagenta", "HiWhite"}
+
+	for i := 0; i < len(StyleCases); i++ {
+		for j := 0; j < len(ColorTypes); j++ {
+			Box := box.New(box.Config{Px: 2, Py: 6, Type: StyleCases[i], Color: ColorTypes[j], ContentColor: "Cyan", TitleColor: [3]uint{215, 58, 74}})
+			fmt.Print(fmt.Sprint("Using ", StyleCases[i], " as Style and ", ColorTypes[j], " as Color:  "))
+			Box.Println("Box CLI Maker 📦", "Highly Customized Terminal	Box Maker")
+		}
+	}
+}
